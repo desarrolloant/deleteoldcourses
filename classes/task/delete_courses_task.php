d339fea65f1875d424c865f37b73739da1de7127
diegofdoruiz <diego.fernando.ruiz@correounivalle.edu.co>
2020-10-07 06:34:24 -0500

    cambios para producción

--- classes/task/delete_courses_task.php
+++ classes/task/delete_courses_task.php
@@ -25,7 +25,7 @@ set_time_limit(300);
 use DateTime;
 
 /*****************************************/
-const COURSES_FOR_QUEUE = 4;
+const COURSES_FOR_QUEUE = 150;
 const DATE_FOR_QUEUE = '2010-12-31 23:59';
 /*****************************************/
 
@@ -111,8 +111,8 @@ class delete_courses_task extends \core\task\scheduled_task {
         //Send email
         $coursesToDelete = $DB->count_records('deleteoldcourses');
 
-        //delete_old_courses_send_email( '66996031' , 'administrador', $coursesToDelete, $this->deleted_courses );
-        //delete_old_courses_send_email( '1144132883' , 'administrador', $coursesToDelete, $this->deleted_courses );
+        delete_old_courses_send_email( '66996031' , 'administrador', $coursesToDelete, $this->deleted_courses );
+        delete_old_courses_send_email( '1144132883' , 'administrador', $coursesToDelete, $this->deleted_courses );
         delete_old_courses_send_email( '1130589899' , 'administrador', $coursesToDelete, $this->deleted_courses);
     }
 
@@ -138,7 +138,7 @@ class delete_courses_task extends \core\task\scheduled_task {
             
             // Run only between 1:00 and 4:00
             if (intval(date('H')) < 1 || intval(date('H')) > 4) {
-                //break;
+                break;
             }
 
             $size = $item->size;
