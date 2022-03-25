57fdfc4bbf6e8ca924745f3d669d7607d2aa9713
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-03-24 19:03:08 -0500

    Tests for course dispatcher

--- tests/course_dispatcher_test.php
+++ tests/course_dispatcher_test.php
@@ -41,11 +41,48 @@ class local_deleteoldcourses_course_dispatcher_testcase extends advanced_testcas
      */
     public function test_course_dispatcher() {
 
+        global $DB;
+
+        $this->resetAfterTest(false);
+
         $coursedispatcher = new course_dispatcher();
 
-        $this->assertInstanceOf(course_distpatcher::class, $coursedispatcher);
-        $this->assertIsInt($coursedispatcher->timecreatedcriteria);
-        $this->assertIsInt($coursedispatcher->timemodifiedcriteria);
+        // Timecreated ok. Timemodified ok.
 
+        $course1 = $this->getDataGenerator()->create_course(array('timecreated' => 1293856998));
+        $course1->timemodified = 1262311998;
+        $DB->update_record('course', $course1);
+
+        $course2 = $this->getDataGenerator()->create_course(array('timecreated' => 1293857998));
+        $course2->timemodified = 1262221998;
+        $DB->update_record('course', $course2);
+
+        // Timecreated ok. Timemodified not.
+        $course3 = $this->getDataGenerator()->create_course(array('timecreated' => 1293851998));
+        $course3->timemodified = 1262331999;
+        $DB->update_record('course', $course3);
+
+        // Timecreated not. Timemodified ok.
+        $course4 = $this->getDataGenerator()->create_course(array('timecreated' => 1293859999));
+        $course4->timemodified = 1252321999;
+        $DB->update_record('course', $course4);
+
+        // Timecreated not. Timemodified not.
+        $course5 = $this->getDataGenerator()->create_course(array('timecreated' => 1293867999));
+        $course5->timemodified = 1262921999;
+        $DB->update_record('course', $course5);
+
+        $this->assertInstanceOf(course_dispatcher::class, $coursedispatcher);
+        $this->assertIsInt($coursedispatcher->get_timecreated_criteria());
+        $this->assertIsInt($coursedispatcher->get_timemodified_criteria());
+        $this->assertIsInt($coursedispatcher->get_limitquery());
+
+        $this->assertEquals(1293857999, $coursedispatcher->get_timecreated_criteria());
+        $this->assertEquals(1262321999, $coursedispatcher->get_timemodified_criteria());
+        $this->assertEquals(5000, $coursedispatcher->get_limitquery());
+
+        $this->assertIsArray($coursedispatcher->get_courses_to_delete());
+
+        $this->assertEquals(2, count($coursedispatcher->get_courses_to_delete()));
     }
 }
