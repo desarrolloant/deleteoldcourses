25637583bb703ac083e3c7f72700cb12796353f6
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-06-21 15:39:08 -0500

    Definition of date manager class

---
+++ classes/date_manager.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Provides the date_manager class.
+ *
+ * @package    local_deleteoldcourses
+ * @since      Moodle 3.10
+ * @author     Iader E. García Gómez <iadergg@gmail.com>
+ * @copyright  2022 Área de Nuevas Tecnologías - Universidad del Valle <desarrollo.ant@correounivalle.edu.co>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_deleteoldcourses;
+
+use stdClass;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Date manager class for the plugin Delete old courses.
+ *
+ * @package    local_deleteoldcourses
+ * @since      Moodle 3.10
+ * @author     Iader E. García Gómez <iadergg@gmail.com>
+ * @copyright  2022 Área de Nuevas Tecnologías - Universidad del Valle <desarrollo.ant@correounivalle.edu.co>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class date_manager {
+}
