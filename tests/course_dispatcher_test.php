c38d0004e147bf0d6005cff27080f21319d25ce0
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-06-30 10:37:30 -0500

    Test course dispatcher

--- tests/course_dispatcher_test.php
+++ tests/course_dispatcher_test.php
@@ -38,27 +38,32 @@ class course_dispatcher_test extends \advanced_testcase {
 
         global $DB;
 
-        $this->resetAfterTest(false);
+        $this->resetAfterTest(true);
 
         $numberofcategoriesexcluded = 4;
         $coursecategoriesexcluded = array();
-        $numberofcoursesexcluded = 50;
-        $numberofcoursesok = 100;
+        $numberofcoursesexcluded = 85;
+        $numberofcoursestodelete = 15;
+
+        $creationtimecriteria = 1293771599; // Thursday, December 30th 2010 23:59:59 GMT-05:00.
+        $lastmodificationtimecriteria = 1357016399; // Monday, December 31th 2012 23:59:59 GMT-05:00
+        $mintimestamp = 1104555600; // Saturday, 1 january 2005 0:00:00 GMT-05:00.
+        $maxtimestamp = 2556161999; // Saturday, 31 december 2050 23:59:59 GMT-05:00.
 
         $configgenerator = $this->getDataGenerator()->get_plugin_generator('local_deleteoldcourses');
 
         // Test environment.
         // Plugin Settings.
-        // Creation date: 01-01-2005. Timestamp local time: 1122872400.
-        $configgenerator->update_setting('year_creation_date', '2005');
-        $configgenerator->update_setting('month_creation_date', '01');
-        $configgenerator->update_setting('day_creation_date', '01');
-        $configgenerator->update_setting('hour_creation_date', '00');
-        $configgenerator->update_setting('minutes_creation_date', '00');
-        $configgenerator->update_setting('seconds_creation_date', '00');
+        // Creation date: 31-12-2010. Timestamp local time: 1293771600.
+        $configgenerator->update_setting('year_creation_date', '2010');
+        $configgenerator->update_setting('month_creation_date', '12');
+        $configgenerator->update_setting('day_creation_date', '31');
+        $configgenerator->update_setting('hour_creation_date', '23');
+        $configgenerator->update_setting('minutes_creation_date', '59');
+        $configgenerator->update_setting('seconds_creation_date', '59');
 
-        // Last modification date: 31-12-2010. Timestamp local time: 1293857999.
-        $configgenerator->update_setting('year_last_modification_date', '2010');
+        // Last modification date: 31-12-2012. Timestamp local time: 1357016399.
+        $configgenerator->update_setting('year_last_modification_date', '2012');
         $configgenerator->update_setting('month_last_modification_date', '12');
         $configgenerator->update_setting('day_last_modification_date', '31');
         $configgenerator->update_setting('hour_last_modification_date', '23');
@@ -81,22 +86,39 @@ class course_dispatcher_test extends \advanced_testcase {
         }
 
         // 15 courses whose creation date is less than the criteria and the modification date is less than the criteria.
+        // Courses type A.
         for ($i = 0; $i < 15; $i++) {
             $course = $this->getDataGenerator()->create_course();
 
-            $course->timecreated = rand(1104555600, 1122872399);
-            $course->timemodified = rand(1104555600, 1122872399);
+            $course->timecreated = rand($mintimestamp, $creationtimecriteria);
+            $course->timemodified = rand($mintimestamp, $lastmodificationtimecriteria);
             $DB->update_record('course', $course);
         };
 
-        // 10 courses whose last modification date is greater than the criteria.
-        for ($i = 0; $i < 10; $i++) {
+        // 20 courses whose creation date is greater than the criteria and the last modification date is less than the criteria.
+        // Courses type B.
+        for ($i = 0; $i < 20; $i++) {
             $course = $this->getDataGenerator()->create_course();
+
+            $course->timecreated = rand($creationtimecriteria, $maxtimestamp);
+            $course->timemodified = rand($mintimestamp, $lastmodificationtimecriteria);
+            $DB->update_record('course', $course);
+        }
+
+        // 40 courses whose creation date is greater than the criteria and the last modification date is greater than the criteria.
+        // Courses type C.
+        for ($i = 0; $i < 40; $i++) {
+            $course = $this->getDataGenerator()->create_course();
+
+            $course->timecreated = rand($creationtimecriteria, $maxtimestamp);
+            $course->timemodified = rand($lastmodificationtimecriteria, $maxtimestamp);
+            $DB->update_record('course', $course);
         }
 
         $coursedispatcher = new course_dispatcher();
-        $course->timecreated = rand(1104555600, 1122872399);
-        $DB->update_record('course', $course);
+        $coursestodelete = $coursedispatcher->get_courses_to_delete();
 
+        $this->assertIsArray($coursestodelete);
+        $this->assertSame(15, count($coursestodelete));
     }
 }
